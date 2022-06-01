b508a79848acbfc829719d0e4c83ba8c10a97bd4
Mxn-ptr <maxence0@hotmail.fr>
2022-06-01 09:42:56 +0200

    Fix task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix the web stack debugging 3
 
 exec { 'Fix a p':
-  path    => [ '/bin/', '/sbin/', '/usr/bin', '/usr/sbin']
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
+  path    => [ '/bin/', '/sbin/', '/usr/bin', '/usr/sbin/']
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
